09a1cc9335364a142391039291727373a5097c08
Cloe Kouadjo <armelfli50@gmail.com>
2019-09-24 06:43:11 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -3,9 +3,7 @@ class Show < ActiveRecord::Base
   has_many :actors, through: :characters
   
   attr_accessor :network
-  
   self.network = Network.find(self.network_id)
-  
   def actors_list 
     self.actors.map {|actor|"#{actor.first_name} #{actor.last_name}"}
     p self
