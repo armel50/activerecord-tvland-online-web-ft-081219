9791c7a0b873385945d93fb6cd4d1b9f8415d452
Cloe Kouadjo <armelfli50@gmail.com>
2019-09-24 06:37:23 +0000

    Automatically backed up by Learn

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,3 +1,33 @@
 class Character < ActiveRecord::Base
+  belongs_to :show 
+  belongs_to :actor
   
+  def say_that_thing_you_say
+    
+     "#{self.name} always says: #{self.catchphrase}"
+  end
+  
+  def build_show(name:)
+
+    found_show =  Show.find_by({name: name})
+     if found_show 
+       self.show = found_show 
+       self.save
+       self
+     else 
+      new_show =  Show.create(name: name) 
+       self.show = new_show  
+       self.save
+       self
+     end
+  end
+  
+  def build_network(call_letters:)  
+    found_net = Network.find_by(call_letters: call_letters) 
+    if found_net
+      
+    else
+      new_net =  Network.create(call_letters: call_letters)
+    end
+  end
 end
